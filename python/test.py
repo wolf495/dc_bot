e08cc5ce1871ad4459cf35ebdc4ecd8c0feb5c66
Alfredo <alfredosoto3598@gmail.com>
2025-09-13 23:35:41 -0400

    adding crunchyroll api support

--- python/test.py
+++ python/test.py
@@ -1,36 +1,60 @@
-import crunpyroll
-import asyncio
+import os,sys,subprocess,asyncio,crunpyroll
+
+
+memory = "C:\\_DEV\\dc_bot\\python\\memory\\crunchyroll"
 
 client = crunpyroll.Client(
     email="MortyNewman94@hotmail.com",
     password="nncBWpIy5",
     locale="en-US"
 )
-async def main():
-    # Start client and login
-    await client.start()
-    # Search for Attack on Titan
-    query = await client.search("My Dress Up Darling")
+        
+
+async def queryLastDubbedEp(queryString):
+    if not client.session.is_authorized:
+        await client.start()
+    query = await client.search(queryString)
     series_id = query.items[0].id
-    print("===SERIESID=====")
-    print(series_id)
-    # Retrieve all seasons of the series
     seasons = await client.get_seasons(series_id)
-    print("===SEASONSID=====")
-    print(seasons)
-    episodes = await client.get_episodes("GY19CPG00")
-    #print(type(episodes))
-    #xx = episodes.items[0]
-    #print(xx)
-    #[a for a in dir(episodes) if not a.startswith('__') and not callable(getattr(episodes, a))]
-    print(episodes.items[0])
+    latestSeason = seasons.items[-1].id
+    episodes = await client.get_episodes(latestSeason)
+    LastDubEp = None
     for ep in episodes.items:
-        strOut = f'S{ep.season_number}E{ep.episode_number}:{ep.title} '
-        if ep.is_dubbed:
-            strOut+=f'[Dubbed]'
-        print(strOut)
-    print("===EPS=====")
-    #print(episodes)
+        if ep.is_dubbed:LastDubEp = ep
+        else:break
+    return LastDubEp
 
+async def checkForNewDubbed():
+    trackedShows = os.listdir(memory)
+    for show in trackedShows:
+        queryStr = os.path.splitext(show)[0]
+        LastDubEp = await queryLastDubbedEp(queryStr)
+        CurLastDubbedStr = f'S{LastDubEp.season_number}E{LastDubEp.episode_number}:{LastDubEp.title}'
+        MemLastDubbedStr = None
+        with open(f'{memory}\\{show}', 'r') as file:
+            MemLastDubbedStr = file.read()
 
-asyncio.run(main())
+        if CurLastDubbedStr != MemLastDubbedStr:
+            command = ['node', '..\\python_announce.js',
+                    CurLastDubbedStr,LastDubEp.season_title.split(' Season ')[0],
+                    LastDubEp.images.thumbnail[0].url,
+                    f'https://www.crunchyroll.com/watch/{LastDubEp.id}/{LastDubEp.slug}']
+            result = subprocess.run(command, capture_output=True, text=True)
+            with open(f'{memory}\\{show}', 'w') as file:
+                file.write(CurLastDubbedStr)
+
+async def addNewShow(newShow):
+    LastDubEp = await queryLastDubbedEp(newShow)
+    if LastDubEp:
+        with open(f'{memory}\\{newShow}.txt', 'w') as file:
+            file.write(f'S{LastDubEp.season_number}E{LastDubEp.episode_number}:{LastDubEp.title}')
+    else:
+        return False
+    return True
+
+if len(sys.argv) > 0 and sys.argv[1].lower() == 'check':
+    asyncio.run(checkForNewDubbed())
+elif len(sys.argv) > 1 and sys.argv[1].lower() == 'add':
+    asyncio.run(addNewShow(sys.argv[2]))
+else:
+    exit(1)
